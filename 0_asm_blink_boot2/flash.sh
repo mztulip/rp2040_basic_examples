118a31583adf0954f7fddcae7c7c3c78c505e023
Mateusz <mateusz@tulip.lol>
2025-01-11 20:53:50 +0100

    info about openocd updated

--- 0_asm_blink_boot2/flash.sh
+++ 0_asm_blink_boot2/flash.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Tested with:
+# Openocd official
 # Open On-Chip Debugger 0.12.0+dev-01829-g250ab1008 (2025-01-06-18:09)
+# Openocd raspberrypi repo version 
+# Open On-Chip Debugger 0.12.0+dev-gebec950 (2025-01-11-12:30)
+
 
 /opt/openocd/bin/openocd \
  -f interface/ftdi/jtag-lock-pick_tiny_2.cfg \
@@ -18,3 +22,6 @@
  # if verification used it always fail when SPI peripheral SSI
  # is configured in normal mode, beause it only supports XIP in such situation
  # Flash memory readings is working only in Quad mode.(perhaps dual also see rp2040  SSI doc)
+ # update verify does noot work with official openocd version https://github.com/openocd-org/openocd branch master
+ # but verify works with raspberrypi openocd version https://github.com/raspberrypi/openocd branch rp2040
+ # both repositories tested with 0.12.0 version
